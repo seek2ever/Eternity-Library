cf7931ef05e606990af7203cf83aaa7ec9e5e1b0
iou0124 <170124.chris@gmail.com>
2023-12-05 21:20:38 +0800

    增加书籍评分方法的相关代码。

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Books:
     阅读状态(尚未阅读、正在阅读、暂停阅读、阅读完成）、阅读进度、阅读时长、内容简介
     """
 
-    def __init__(self, book_name, author, publisher="", publication_date="", pages="", grade="",
+    def __init__(self, book_name, author, publisher="", publication_date="", pages="", level="",
                  isbn="", translator="", nationality="", book_type="",
                  reading_status="", reading_time="", reading_progress="", introduction=""):
         self.book_name = book_name
@@ -16,7 +16,7 @@ class Books:
         self.publisher = publisher
         self.publication_date = publication_date
         self.pages = pages
-        self.grade = grade
+        self.level = level
         self.isbn = isbn
         self.translator = translator
         self.nationality = nationality
@@ -26,7 +26,7 @@ class Books:
         self.reading_progress = reading_progress
         self.introduction = introduction
 
-        print(f"书籍名称：{self.book_name}\n作者：{self.author}\n出版社：{self.publisher}\n评分：{self.grade}")
+        print(f"书籍名称：{self.book_name}\n作者：{self.author}\n出版社：{self.publisher}\n评分：{self.level}")
 
     def book_animation(self):
         """控制电子书打开、关闭、翻页时的动画效果"""
@@ -58,6 +58,11 @@ class Books:
             self.reading_status = books_status[-1]             # 将阅读状态设置为“阅读完成”
         else:
             self.reading_status = input()                      # 由用户从给定的列表中选择阅读状态
+
+    def level(self):
+        """统计书籍的评分情况，以一到五个⭐表示，分别表示书籍由低到高的评价"""
+        level_symbol = ['⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐']
+        set_level = input()
 
 
 history_book = Books("Python编程：从入门到实践", "埃里克·马瑟斯", nationality="美国")
