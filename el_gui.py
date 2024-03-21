1dd0b1431faf9f92974e0a0a826ed959992bde92
iou0124 <170124.chris@gmail.com>
2024-03-22 00:17:16 +0800

    设置gui界面图标；添加一个关闭按钮。

--- el_gui.py
+++ el_gui.py
@@ -1,8 +1,8 @@
-import os
 import sys
-from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QDesktopWidget, QPushButton, QLabel, QFileDialog, QVBoxLayout, QMessageBox
-from PyQt5.QtCore import Qt, QCoreApplication
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
 class MyWindow(QMainWindow):
@@ -14,9 +14,16 @@ class MyWindow(QMainWindow):
 
     def setup_ui(self):
         # 创建主窗口
-        self.setObjectName("MainWindow")                                    # 设置窗口的对象名称
-        self.setWindowTitle(self._translate("Eternity Library", "三木书斋"))  # 设置窗口的标题名称
-        self.setGeometry(200, 200, 1920, 1080)                              # 设置窗口的位置和大小
+        self.setObjectName("MainWindow")                                # 设置窗口的对象名称
+        self.setWindowTitle(self._translate("Eternity Library", "三木书斋"))        # 设置窗口的标题名称
+        self.setGeometry(200, 200, 1920, 1080)                          # 设置窗口的位置和大小
+        self.setWindowOpacity(1)                                        # 设置窗口的透明度
+
+        # 设置图标
+        icon = QtGui.QIcon()                                           # 创建一个图标对象
+        icon.addPixmap(QtGui.QPixmap("images/book_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)                                       # 设置窗口的图标
+
         # 创建菜单栏
         self.menubar = QtWidgets.QMenuBar(self)                         # 创建一个菜单栏，分配实际的QtWidgets.QMenuBar对象
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))           # 设置菜单栏的位置和大小
@@ -26,6 +33,12 @@ class MyWindow(QMainWindow):
         self.statusbar = QtWidgets.QStatusBar(self)                     # 创建一个状态栏，分配实际的QtWidgets.QStatusBar对象
         self.statusbar.setObjectName("statusbar")                       # 设置状态栏的对象名称
         self.setStatusBar(self.statusbar)                               # 设置窗口的状态栏
+        # 添加关闭按钮
+        self.pushButton = QtWidgets.QPushButton(self)                   # 创建一个按钮，分配实际的QtWidgets.QPushButton对象
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 100, 40))        # 设置按钮的位置和大小
+        self.pushButton.setObjectName("pushButton")                     # 设置按钮的对象名称
+        self.pushButton.setText(self._translate("MainWindow", "关闭"))    # 设置按钮的文本
+        self.pushButton.clicked.connect(self.close)                     # 设置按钮的点击事件
 
     def _translate(self, context, text):
         """翻译标题栏文本"""
