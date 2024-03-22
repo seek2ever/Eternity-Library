10217dcfbbeff5c4ae586da26b1f16903b565b08
iou0124 <170124.chris@gmail.com>
2024-03-22 22:11:36 +0800

    增加扫描文件按钮，调用ScanBookFiles中的select_directory方法，扫描硬盘中的文件；目前已知bug：第一次扫描完成后，如果再次点击扫描会出现：“在扫描文件时发生错误: name 'scan_book_files' is not defined”的问题。

--- el_gui.py
+++ el_gui.py
@@ -1,5 +1,5 @@
 import sys
-
+from books import ScanBookFiles
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 class MyWindow(QMainWindow):
     def __init__(self):                         # 初始化窗口
         super().__init__()                      # 调用父类的初始化方法
+        self.scanButton = None
+        self.pushButton = None
         self.statusbar = None
         self.menubar = None
         self.setup_ui()                         # 设置窗口的属性
@@ -20,29 +22,43 @@ class MyWindow(QMainWindow):
         self.setWindowOpacity(1)                                        # 设置窗口的透明度
 
         # 设置图标
-        icon = QtGui.QIcon()                                           # 创建一个图标对象
+        icon = QtGui.QIcon()                                            # 创建一个图标对象
         icon.addPixmap(QtGui.QPixmap("images/book_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.setWindowIcon(icon)                                       # 设置窗口的图标
+        self.setWindowIcon(icon)                                        # 设置窗口的图标
 
         # 创建菜单栏
         self.menubar = QtWidgets.QMenuBar(self)                         # 创建一个菜单栏，分配实际的QtWidgets.QMenuBar对象
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))           # 设置菜单栏的位置和大小
         self.menubar.setObjectName("menubar")                           # 设置菜单栏的对象名称
         self.setMenuBar(self.menubar)                                   # 设置窗口的菜单栏
+
         # 创建状态栏
         self.statusbar = QtWidgets.QStatusBar(self)                     # 创建一个状态栏，分配实际的QtWidgets.QStatusBar对象
         self.statusbar.setObjectName("statusbar")                       # 设置状态栏的对象名称
         self.setStatusBar(self.statusbar)                               # 设置窗口的状态栏
+
         # 添加关闭按钮
         self.pushButton = QtWidgets.QPushButton(self)                   # 创建一个按钮，分配实际的QtWidgets.QPushButton对象
-        self.pushButton.setGeometry(QtCore.QRect(10, 10, 100, 40))        # 设置按钮的位置和大小
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 100, 40))      # 设置按钮的位置和大小
         self.pushButton.setObjectName("pushButton")                     # 设置按钮的对象名称
-        self.pushButton.setText(self._translate("MainWindow", "关闭"))    # 设置按钮的文本
+        self.pushButton.setText(self._translate("Close", "关闭"))    # 设置按钮的文本
         self.pushButton.clicked.connect(self.close)                     # 设置按钮的点击事件
+
+        # 添加扫描文件按钮
+        self.scanButton = QtWidgets.QPushButton(self)
+        self.scanButton.setGeometry(QtCore.QRect(120, 10, 150, 40))
+        self.scanButton.setObjectName("scanButton")
+        self.scanButton.setText(self._translate("Scan Files", "扫描文件"))
+        self.scanButton.clicked.connect(self.scan_books)                # 设置按钮的点击事件
 
     def _translate(self, context, text):
         """翻译标题栏文本"""
         return QCoreApplication.translate(context, text)
+
+    def scan_books(self):
+        """扫描文件按钮点击事件，调用ScanBookFiles类的select_directory方法"""
+        scan_dialog = ScanBookFiles()                      # 创建ScanBookFiles类的实例
+        scan_dialog.select_directory()                     # 调用select_directory方法
 
 
 if __name__ == '__main__':
