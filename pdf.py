f01b9718f474cf28162af5c1ed16272d82077997
iou0124 <170124.chris@gmail.com>
2025-04-08 21:05:06 +0800

    fix: 修复get_pdf_path函数无法正确获取书籍路径的问题。

--- pdf.py
+++ pdf.py
@@ -1,20 +1,31 @@
 # 用于实现pdf编辑、转换、合并、提取、压缩等操作
-
+import os
 import fitz
+from database import DatabaseManager
 
 
-def get_pdf_path(self):
-    """获取pdf文件的路径"""
-    with open("scan_record.json") as files:
-        for file in files:
-            return file
+def get_pdf_path(book_name: str):
+    """
+    获取pdf文件的路径
+    :param book_name: 需要获取路径的书籍名称
+    """
+    path = DatabaseManager()
+    file_path = path.get_book(book_name, columns='book_path')  # 获取pdf文件的路径
+    return file_path
 
 
 class PDFEdit:
-    def get_pdf_character(self):
+    def get_pdf_character(self, book_name: str):
         """获取pdf文件每页的文字"""
-        get_path = self.get_pdf_path()  # 获取pdf文件的路径
+        get_path = get_pdf_path(book_name)
         pdf_doc = fitz.open(get_path)
+        full_text = ""  # 初始化一个空字符串来存储所有文本
         for page in pdf_doc:
             text = page.get_text()
-            print(text)
+            full_text += text  # 累加每一页的文本
+        return full_text  # 返回所有文本
+
+
+if __name__ == '__main__':
+    test_2 = PDFEdit().get_pdf_character('test_file.pdf')
+    print(test_2)
