949afc0f9d10efc46b37baaf8ebea161b2302345
iOU-Assistant <iou170124@outlook.com>
2025-04-09 17:29:51 +0800

    feat: get_pdf_path方法中增加对文件路径存在与否的判断功能。

--- pdf.py
+++ pdf.py
@@ -8,6 +8,7 @@ def get_pdf_path(book_name: str) -> str:
     """
     :param book_name: 需要获取路径的书籍名称
     :return: pdf文件的路径
+    :raise: FileNotFoundError 文件不存在时抛出错误
     """
     path = DatabaseManager()
     file_path = path.get_book(
@@ -15,6 +16,8 @@ def get_pdf_path(book_name: str) -> str:
         columns='book_path'
     )  # 获取pdf文件的路径,返回的格式为列表，如：[('F:/Books',)]
     file_path = file_path[0][0]  # 将列表转换为字符串，便于后续操作
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"路径 {file_path} 不存在，请检查文件路径是否正确！")
     return file_path
 
 
