98ab060df6fcd5ef67d0c336b95d4ca55cab0f39
iou0124 <170124.chris@gmail.com>
2025-04-09 15:18:13 +0800

    refactor: 将get_pdf_character重命名为get_pdf_text，并将其设置为静态方法；增加get_pdf_image方法，尚未实现相应功能。

--- pdf.py
+++ pdf.py
@@ -4,28 +4,33 @@ import fitz
 from database import DatabaseManager
 
 
-def get_pdf_path(book_name: str):
+def get_pdf_path(book_name: str) -> str:
     """
-    获取pdf文件的路径
     :param book_name: 需要获取路径的书籍名称
+    :return: pdf文件的路径
     """
     path = DatabaseManager()
-    file_path = path.get_book(book_name, columns='book_path')  # 获取pdf文件的路径
+    file_path = path.get_book(
+        book_name,
+        columns='book_path'
+    )  # 获取pdf文件的路径,返回的格式为列表，如：[('F:/Books',)]
+    file_path = file_path[0][0]  # 将列表转换为字符串，便于后续操作
     return file_path
 
 
 class PDFEdit:
-    def get_pdf_character(self, book_name: str):
+    @staticmethod
+    def get_pdf_text(book_name: str):
         """获取pdf文件每页的文字"""
-        get_path = get_pdf_path(book_name)
-        pdf_doc = fitz.open(get_path)
-        full_text = ""  # 初始化一个空字符串来存储所有文本
-        for page in pdf_doc:
-            text = page.get_text()
-            full_text += text  # 累加每一页的文本
-        return full_text  # 返回所有文本
+        path = get_pdf_path(book_name)
+        pdf = fitz.open(os.path.join(path, book_name))
+        for page in pdf:
+            print(page.get_text())
+        pdf.close()
+
+    def get_pdf_image(self, book_name: str):
+        """获取pdf文件每页的图片"""
 
 
 if __name__ == '__main__':
-    test_2 = PDFEdit().get_pdf_character('test_file.pdf')
-    print(test_2)
+    PDFEdit.get_pdf_text('test_file.pdf')
