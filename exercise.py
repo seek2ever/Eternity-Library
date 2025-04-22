b3ef0e9354f11c12f6a6049ae0f4e3e9158d99a5
iOU0225 <170124.chris@gmail.com>
2025-04-22 15:20:02 +0800

    feat: 修改为PyQt5窗口练习代码。

--- exercise.py
+++ exercise.py
@@ -1,17 +1,44 @@
-import time
-from tqdm import tqdm, trange
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QWidget, QApplication, QVBoxLayout, QPushButton, QDesktopWidget, \
+    QLabel, QHBoxLayout
+from PyQt5.QtCore import Qt
 
 
-for i in trange(100):
-    time.sleep(0.01)
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("click to jump")
+        self.setGeometry(1000, 500, 500, 300)
 
-for i in tqdm(range(100), desc='processing'):
-    time.sleep(0.05)
+        self.push_button = QPushButton("click to jump", self)
+        self.push_button.clicked.connect(self.jump)
+        self.push_button.setFixedSize(200, 100)
 
-dic = ['a', 'b', 'c', 'd', 'e']
-pbar = tqdm(dic)
-for i in pbar:
-    pbar.set_description('processing' + i)
-    time.sleep(0.2)
+        self.userLabel = QLabel()
+        self.userLabel.setAlignment(Qt.AlignCenter)
+        self.userLabel.setFixedSize(150, 150)
+        self.userLabel.setText("<a href='https://www.baidu.com'>百度一下</a>")
+        self.userLabel.setOpenExternalLinks(True)
 
-print("hello")
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(self.userLabel)
+        hLayout.addWidget(self.push_button)
+
+        self.layout = QVBoxLayout()
+        self.layout.addLayout(hLayout)
+
+        mainLayout = QWidget()
+        mainLayout.setLayout(self.layout)
+        self.setCentralWidget(mainLayout)
+
+        screen = QDesktopWidget().screenGeometry()
+    def jump(self):
+        QMessageBox.information(self, "提示", "成功点击！")
+
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
