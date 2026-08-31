00e3c0ef0954139b68b165f805afc95481a9dab7
Chris <170124.chris@gmail.com>
2026-08-31 11:11:42 +0800

    Add extra drag-and-drop example

--- extra.py
+++ extra.py
@@ -13,8 +13,10 @@ class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(600, 600)
+        self.setAcceptDrops(True)
         self.btn = QPushButton("拖拽我", self)
         self.btn.setGeometry(QRect(100, 100, 100, 30))
+        self.btn.setObjectName("my_btn")
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -23,7 +25,21 @@ class MyWindow(QWidget):
             drag.setHotSpot(self._drag_hotspot)
 
             mime = QMimeData()
-            mime.setText()
+            mime.setText("my_btn")
+            drag.setMimeData(mime)
+            drag.exec(Qt.MoveAction)
+    
+    def dropEnterEvent(self, event):
+        if event.mimeData().hasText():
+            event.acceptProposedAction()
+        else:
+            event.ignore()
+    
+    def dragMoveEvent(self, event):
+        pass
+
+    def dropEvent(self, event):
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
